b8455dc1adf80d7fccb16dcfb46db661f8d0b3dc
Abed Nganga <ngangaabed@gmail.com>
2023-06-15 13:07:35 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+
+  function returnsANamedFunction() {
+   return function receivesAFunction() {
+   };
+  };
+
+  function returnsAnAnonymousFunction () {
+    return function() {};
+  };
